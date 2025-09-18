149bc67a4075cb09a807355939b2b68e9e1ba602
abdulalilm1010 <abdulalim243457@gmail.com>
2025-09-18 10:18:17 +0600

    Work in progress: TopRated section changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Featured from "@/components/Featured";
+import TopRated from "@/components/TopRated";
+
 
 export default function Home() {
   return (
     <div>
        <Hero/>
-       <Featured />
+      <Featured />
+      <TopRated/>
     </div>
   );
 }
