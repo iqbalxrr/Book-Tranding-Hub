e1a5770e4895de542e78fb4dad5811f4365463e2
SheikhSalman <assalmanmuhammad@gmail.com>
2025-09-22 16:40:24 +0600

    book information card

--- src/app/detailes/page.js
+++ src/app/detailes/page.js
@@ -84,7 +84,7 @@ const DetailesPage = () => {
 
                 {/* right div  */}
                 <div className='flex-5/9'>
-                
+
                     <div className="py-6">
                         <h2 className="text-4xl font-bold mb-4">This is a Heading</h2>
                         <p className="text-gray-600 mb-6">
@@ -102,14 +102,55 @@ const DetailesPage = () => {
                             </button>
                         </div>
                     </div>
-                            {/* buttons  */}
+                    {/* buttons  */}
                     <div className='flex items-center gap-3'>
-                    <button className="w-12 h-12 flex justify-center items-center text-white hover:text-[#FF7B6B] transition duration-700 p-2 rounded-full  bg-[#FF7B6B] hover:bg-[#FFEFEF] border hover:border-[#FF7B6B]">
-                        <RiPokerHeartsLine />
-                    </button>
-                    <button className="w-12 h-12 flex justify-center items-center hover:text-white transition duration-700 p-2 rounded-full border border-gray-400 hover:bg-[#FF7B6B]">
-                        <TbArrowsCross />
-                    </button>
+                        <button className="w-12 h-12 flex justify-center items-center text-white hover:text-[#FF7B6B] transition duration-700 p-2 rounded-full  bg-[#FF7B6B] hover:bg-[#FFEFEF] border hover:border-[#FF7B6B]">
+                            <RiPokerHeartsLine />
+                        </button>
+                        <button className="w-12 h-12 flex justify-center items-center hover:text-white transition duration-700 p-2 rounded-full border border-gray-400 hover:bg-[#FF7B6B]">
+                            <TbArrowsCross />
+                        </button>
+                    </div>
+
+                    <hr className="my-6 border-t border-gray-200" />
+
+                    <div className='border border-gray-200 rounded-2xl p-2'>
+                        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4  bg-gray-50 p-6 rounded-2xl">
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">SKU:</h3>
+                                <p className="text-gray-600">FTC1020B65D</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Tage</h3>
+                                <p className="text-gray-600">Low Book</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Total Pages</h3>
+                                <p className="text-gray-600">360</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Pulish Year</h3>
+                                <p className="text-gray-600">2023</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Category</h3>
+                                <p className="text-gray-600">Kids Toys</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Format</h3>
+                                <p className="text-gray-600">Hardcover</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Language</h3>
+                                <p className="text-gray-600">English</p>
+                            </div>
+                            <div >
+                                <h3 className="text-xl font-semibold mb-2">Century</h3>
+                                <p className="text-gray-600">United States</p>
+                            </div>
+
+
+                        </div>
                     </div>
 
                 </div>
