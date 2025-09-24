18618e0df0d9764109c18b0cd78a99c5b28fba73
iqbalxrr <iamekbal7500@gmail.com>
2025-09-24 23:02:12 +0600

    added books add get api

--- src/components/FeaturedBooks.jsx
+++ src/components/FeaturedBooks.jsx
@@ -1,5 +1,5 @@
-'use client'
-
+"use client";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
@@ -7,66 +7,90 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "../styles/variables.css";
 
-const books = [
-  {
-    image: "/book1.png",
-    author: "Author One",
-    title: "Book One",
-    reviewerImg: "/book1.png",
-    reviewer: "Reviewer One",
-    rating: "⭐⭐⭐⭐",
-  },
-  {
-    image: "/book2.png",
-    author: "Author Two",
-    title: "Book Two",
-    reviewerImg: "/book1.png",
-    reviewer: "Reviewer Two",
-    rating: "⭐⭐⭐",
-  },
-  {
-    image: "/book3.png",
-    author: "Author Three",
-    title: "Book Three",
-    reviewerImg: "/book1.png",
-    reviewer: "Reviewer Three",
-    rating: "⭐⭐⭐⭐⭐",
-  },
-  {
-    image: "/book4.png",
-    author: "Author Four",
-    title: "Book Four",
-    reviewerImg: "/book1.png",
-    reviewer: "Reviewer Four",
-    rating: "⭐⭐⭐",
-  },
-];
+const FeaturedBooks = () => {
+  const books = [
+    {
+      id: 1,
+      title: "The Hidden Mystery Behind",
+      author: "Hawkins",
+      price: 29.0,
+      image: "/book1.png",
+      reviewer: "Hawkins",
+      rating: "⭐⭐⭐⭐☆",
+    },
+    {
+      id: 2,
+      title: "Qple GPad With Retina Sisplay",
+      author: "Albert",
+      price: 19.0,
+      image: "/book2.png",
+      reviewer: "Albert",
+      rating: "⭐⭐⭐⭐⭐",
+    },
+    {
+      id: 3,
+      title: "Flovely And Unicorn Erna",
+      author: "Alexander",
+      price: 30.0,
+      image: "/book3.png",
+      reviewer: "Alexander",
+      rating: "⭐⭐⭐⭐☆",
+    },
+    {
+      id: 4,
+      title: "Simple Things You To Save BOOK",
+      author: "Wilson",
+      price: 30.0,
+      oldPrice: 39.99,
+      image: "/book4.png",
+      reviewer: "Wilson",
+      rating: "⭐⭐⭐⭐☆",
+    },
+    {
+      id: 5,
+      title: "How Deal With Very Bad BOOK",
+      author: "Esther",
+      price: 39.0,
+      image: "/book3.png",
+      reviewer: "Esther",
+      rating: "⭐⭐⭐⭐☆",
+    },
+    {
+      id: 6,
+      title: "How Deal With Very Bad BOOK",
+      author: "Esther",
+      price: 39.0,
+      image: "/book1.png",
+      reviewer: "Esther",
+      rating: "⭐⭐⭐⭐☆",
+    },
+  ];
 
-export default function FeaturedBooks() {
   return (
-    <div className="my-20 px-7 md:px-14 lg:px-20 mx-auto container space-y-12">
-      {/* heading */}
-      <div className="flex justify-between items-center">
-        <h1 className="text-5xl font-bold">Featured Books</h1>
-        <button className="flex items-center gap-3 rounded-full font-bold py-2.5 px-5 text-white bg-[#FF7B6B] transition duration-700">
-          Explore More
-            <FaArrowRightLong />
+    <div className=" container mx-auto my-16 px-4">
+      {/* Title and Button */}
+      <div className="flex justify-between items-center mb-8">
+        <h2 className="text-3xl md:text-4xl font-bold">Featured Books</h2>
+        <button className="bg-[#FF7B6B] text-white px-6 py-2 rounded-full hover:bg-[#ff6450] transition">
+          View All
         </button>
       </div>
 
-      {/* Swiper */}
+      {/* Swiper Slider */}
       <Swiper
-        modules={[Autoplay, Pagination]}
-        slidesPerView={1}
         spaceBetween={20}
-        autoplay={{ delay: 4000 }}
-        speed={2000}
-        loop={true}
-        pagination={{ clickable: true }}
+        slidesPerView={1}
         breakpoints={{
-          768: { slidesPerView: 2 }, // md
-          1024: { slidesPerView: 3 }, // lg
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+          1440: { slidesPerView: 5 },
         }}
+        autoplay={{ delay: 3000,
+            disableOnInteraction: false,
+         }}
+         speed={1200}
+        loop={true}
+         modules={[Autoplay, Pagination, Navigation]}
       >
         {books.map((book) => (
           <SwiperSlide key={book.id}>
@@ -97,7 +121,7 @@ export default function FeaturedBooks() {
               </div>
 
               {/* Button */}
-              <button className="w-full btn rounded-full font-bold py-3  ">
+              <button className="w-full custom-btn rounded-full font-bold py-3  ">
                 Exchange
               </button>
             </div>
@@ -106,4 +130,6 @@ export default function FeaturedBooks() {
       </Swiper>
     </div>
   );
-}
+};
+
+export default FeaturedBooks;
