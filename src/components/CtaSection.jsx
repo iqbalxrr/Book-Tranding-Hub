4ae94f4abb4fe30d351fec0f99855600e49f1adc
SheikhSalman <assalmanmuhammad@gmail.com>
2025-09-26 15:00:58 +0600

    fixed z-index issue

--- src/components/CtaSection.jsx
+++ src/components/CtaSection.jsx
@@ -9,7 +9,7 @@ export default function CtaSection() {
             <img className='absolute top-0 left-4 hidden md:block' src="/cta-book-2.png" alt="" />
 
             <div className='w-full lg:w-auto h-full lg:h-auto flex flex-col justify-center items-center text-center space-y-5'>
-                <h1 className='text-3xl font-bold text-white z-50'>Give a Book, Get a Book <br />Join the Book Exchange Buzz </h1>
+                <h1 className='text-3xl font-bold text-white'>Give a Book, Get a Book <br />Join the Book Exchange Buzz </h1>
                 <Link href='/allBooks'>
                     <button
                         className='px-7 py-3 rounded-full bg-white font-bold flex items-center gap-3 hover:bg-black hover:text-white hover:border-1 hover:border-white transition duration-700'
